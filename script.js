b23cb83590fcb3966390e090900b9ba2d26ece03
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-04-08 19:54:41 -0300

    Creating sobre.html segment

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const main = document.getElementById('main');
-    main.style.opacity = '1';
-});
+const content = document.getElementById('content');
+let actualSegment;
+
+sobre();
+
+function sobre() {
+    if(actualSegment !== 'sobre') {
+
+        actualSegment = 'sobre';
+
+        content.innerHTML =
+        '<div class="sobre">' +
+            '<img class="avatar-img" src="https://avatars.githubusercontent.com/u/79710675?v=4" alt="Profile Picture">' +
+            '<div class="content">' +
+                '<h1>Olá devs!</h1>' +
+                '<p> -> Eu sou João Érik da Silva Crisóstomo, estudante de Desenvolvimento de Sistemas.</p>' +
+                '<h3>Habilidades</h3>' +
+                '<h2>-> Linguagens</h2>' +
+                '<p>Java, Javascript, Typescript, Kotlin, </p>' +
+                '<h2>-> FrameWorks</h2>' +
+                '<p>Bootstrap, Springboot, Angular, Ionic</p>' +
+            '</div>' +
+        '</div>';
+
+        fadeInContent();
+    }
+}
+function dados() {}
+function projetos() {}
+function tecnologias() {}
+
+function fadeInContent() {
+    content.style.opacity = '1';
+}
