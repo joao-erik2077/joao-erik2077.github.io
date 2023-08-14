5df6cb8dbceb61c909d9f5d07846fd1da8675a5f
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2023-08-14 19:55:35 -0300

    Added home icon to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import styles from './Navbar.module.scss';
 import { Link, useLocation } from 'react-router-dom';
+import { GoHomeFill } from 'react-icons/go';
 
 export default function Navbar() {
     const location = useLocation();
@@ -9,6 +10,9 @@ export default function Navbar() {
                 <Link to='/aboutme' className={`${styles.link} ${location.pathname === '/aboutme' ? styles.highlight : ''}`}>Sobre Mim</Link>
                 <Link to='/projects' className={`${styles.link} ${location.pathname === '/projects' ? styles.highlight : ''}`}>Projetos</Link>
                 <Link to='/contact' className={`${styles.link} ${location.pathname === '/contact' ? styles.highlight : ''}`}>Contato</Link>
+                <Link to='/' >
+                    <GoHomeFill className={`${styles.link} ${location.pathname === '/' ? styles.highlight : ''}`}/>
+                </Link>
             </div>
         </nav>
     </>;
