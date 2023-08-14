51bda0a8d2925bde90a157697b77639035b26ca0
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2023-08-14 18:48:27 -0300

    Home page built

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,26 @@
 import { Component } from 'react';
 import styles from './Home.module.scss';
 import Navbar from '../../components/Navbar/Navbar';
+import Avatar from '../../assets/avatar.png';
 
 export default class Home extends Component {
+
     render() {
-        return (
+        return <>
             <Navbar />
-        );
+            <main className={styles.main}>
+                <section>
+                    <div className={styles.container}>
+                        <h1 className={styles.title}>Oi, eu sou o João Érik</h1>
+                        <p className={styles.text}>
+                            Desenvolvedor por paixão, apreciador da matemática e entusiasta das línguas.
+                        </p>
+                    </div>
+                </section>
+                <aside>
+                    <img src={Avatar} alt="Foto de perfil" />
+                </aside>
+            </main>
+        </>;
     }
 }
