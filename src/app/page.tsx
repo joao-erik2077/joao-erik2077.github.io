993eba335c395d6c7620d7a29aaedcf6eeded69d
joao-erik2077 <joaoerik2074@gmail.com>
2024-11-05 21:51:21 -0300

    Components folder for future use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => (
-  <div className="min-w-[100vw] min-h-[100vh] flex items-center justify-center">
-    <h1 className="text-5xl">Hello World!</h1>
-  </div>
+  <>
+    <div className="min-w-[100vw] min-h-[100vh] flex items-center justify-center">
+      <h1 className="text-5xl">Hello World!</h1>
+    </div>
+  </>
 );
 
 export default Home;
