6afaefb4376dc6aa044556a9649083c302ad8cc0
joao-erik2077 <joaoerik2074@gmail.com>
2025-02-06 21:02:21 -0300

    Hello, world.

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => (
-  <>
-    <div className="min-w-[100vw] min-h-[100vh] flex items-center justify-center">
-      <h1 className="text-5xl">Hello World!</h1>
-    </div>
-  </>
-);
-
-export default Home;
