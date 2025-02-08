2a0b39dc4dd5ddae7edaf4ae3f07e0e797a4ad98
joao-erik2077 <joaoerik2074@gmail.com>
2025-02-07 23:35:23 -0300

    animation on button click

---
+++ script.js
@@ -0,0 +1,19 @@
+const helloWorldContainer = document.getElementById("hello-world-ctn");
+const infoContainer = document.getElementById("info-ctn");
+
+helloWorldContainer.addEventListener("click", () => {
+  helloWorldContainer.style.border = "1px solid #00ff00";
+  helloWorldContainer.style.padding = "16px";
+  helloWorldContainer.style.color = "#00ff00";
+  changeAnimation(helloWorldContainer, infoContainer);
+});
+
+const changeAnimation = (previous, next) => {
+  previous.style.opacity = "0";
+
+  setTimeout(() => {
+    previous.style.display = "none";
+    next.style.display = "flex";
+    next.style.opacity = "1";
+  }, 1300);
+};
